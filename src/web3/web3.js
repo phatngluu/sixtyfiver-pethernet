1cea7484eb1cc87dc16fa413aedf6a112b1b411d
phatngluu <yadsendew@protonmail.com>
2021-08-11 20:25:43 +0700

    move to folder web3

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const Web3 = require("web3");
 const web3Provider = new Web3.providers.HttpProvider(process.env.PETHERNET_URI);
 const web3 = new Web3(web3Provider);
-const PethernetContractMeta = require('./contracts/PethernetContract.json');
+const PethernetContractMeta = require('../contracts/PethernetContract.json');
 
 module.exports = { 
     web3, 
