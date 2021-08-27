337a36762275bd3355ade0c136c591e16b77060f
phatngluu <yadsendew@protonmail.com>
2021-08-27 20:27:39 +0700

    change user passwords

--- src/auth/user.service.js
+++ src/auth/user.service.js
@@ -5,14 +5,14 @@ const Role = require('../_helpers/role');
 console.log(Role.MinistryOfHealth);
 
 const users = [
-    { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', role: Role.Admin },
-    { id: 2, username: 'ministry', password: '12345', firstName: 'Ministry of Health', role: Role.MinistryOfHealth },
-    { id: 3, username: 'med1', password: '12345', firstName: 'med1', role: Role.MedicalUnit },
-    { id: 4, username: 'med2', password: '12345', firstName: 'med2', role: Role.MedicalUnit },
-    { id: 5, username: 'med3', password: '12345', firstName: 'med3', role: Role.MedicalUnit },
-    { id: 6, username: 'user1', password: '12345', firstName: 'user1', role: Role.Injector },
-    { id: 7, username: 'user2', password: '12345', firstName: 'user2', role: Role.Injector },
-    { id: 8, username: 'user3', password: '12345', firstName: 'user3', role: Role.Injector }
+    { id: 1, username: 'admin', password: 'memphis13322', firstName: 'Admin', role: Role.Admin },
+    { id: 2, username: 'ministry', password: 'memphis13322', firstName: 'Ministry of Health', role: Role.MinistryOfHealth },
+    { id: 3, username: 'med1', password: 'memphis13322', firstName: 'med1', role: Role.MedicalUnit },
+    { id: 4, username: 'med2', password: 'memphis13322', firstName: 'med2', role: Role.MedicalUnit },
+    { id: 5, username: 'med3', password: 'memphis13322', firstName: 'med3', role: Role.MedicalUnit },
+    { id: 6, username: 'user1', password: 'memphis13322', firstName: 'user1', role: Role.Injector },
+    { id: 7, username: 'user2', password: 'memphis13322', firstName: 'user2', role: Role.Injector },
+    { id: 8, username: 'user3', password: 'memphis13322', firstName: 'user3', role: Role.Injector }
 ];
 
 module.exports = {
