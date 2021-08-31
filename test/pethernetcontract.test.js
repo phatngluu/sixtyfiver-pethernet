d80fc39773e308ad1202d1620d4c6c181a090db7
phatngluu <yadsendew@protonmail.com>
2021-08-31 09:12:43 +0700

    add tests for smart contract

--- test/pethernetcontract.test.js
+++ test/pethernetcontract.test.js
@@ -1,15 +1,36 @@
+const { assert } = require("chai");
+
 require("dotenv").config();
 
 const PethernetContract = artifacts.require("PethernetContract");
 
 contract("PethernetContract", (accounts) => {
-    it ('should create vaccine dose', async () => {
-        const instance = await PethernetContract.deployed();
-        ministryAddress = accounts.find(x => x == process.env.MINISTRY_OF_HEALTH_ADDRESS);
+    // first contract is used to deploy contracts to
+    // this is also ministry address
+    const MINISTRY_OF_HEALTH_ADDRESS = accounts[0];
 
+    it ('should deployed contract', async () => {
+        const instance = await PethernetContract.deployed();
+        ministryAddress = accounts[0];
+        assert.isNotNull(instance);
+    })
+
+    it ('should match ministry of health address', async () => {
+        const instance = await PethernetContract.deployed();
+        const contractOwnerAddr = await instance.ministryOfHealthAddr();
+        assert.equal(contractOwnerAddr, MINISTRY_OF_HEALTH_ADDRESS);
+    })
+
+    it ('should create vaccine dose', async () => {
+        // Arrange
+        const instance = await PethernetContract.deployed();
         const vaccineDoseHash = "abc";
-        const result = await instance.addVaccineDose(vaccineDoseHash);
-        console.log(result);
-        asssert.true(instance.checkVaccineDose(vaccineDoseHash));
+        await instance.addVaccineDose(vaccineDoseHash);
+        
+        // Act
+        const result = await instance.checkVaccineDose(vaccineDoseHash);
+
+        // Assert
+        assert.equal(MINISTRY_OF_HEALTH_ADDRESS, result);
     });
 });
